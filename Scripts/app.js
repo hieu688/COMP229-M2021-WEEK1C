62d49a163c0552052593af6be5bcc13336e8c52e
Duy Hieu Nguyen <hieunguyenfph@gmail.com>
2021-05-14 19:17:56 -0400

    Initial commit

---
+++ Scripts/app.js
@@ -0,0 +1,14 @@
+//IIFE -- Immediate Invoked Function Expression
+"use strict";
+(functon(){
+
+    let myVariable=20;
+    console.log("my number is:"+myVariable);
+
+    for(let index=0;index<myVariable;index++){
+    console.log("your name is"+index);
+    }
+
+    console.log(index);
+
+})();
